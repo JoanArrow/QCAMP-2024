54f09d2ca70fa8cb318025eebc54b86ca89adf53
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2024-07-19 15:07:48 -0700

    moved to gui (and dont need press enter)

--- QGame/GatesGame.java
+++ QGame/GatesGame.java
@@ -1,11 +1,19 @@
 package QGame;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Point;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class GatesGame {
-    private Scanner input;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class GatesGame implements Runnable {
     private Space[][] grid;
     private String correctAnswer;
     private String[] nonSpAnswers;
@@ -15,10 +23,12 @@ public class GatesGame {
     private int layers;
     private int rows;
     private int lives;
+    private JFrame frame;
+    private JTextArea text;
+    private JPanel panel;
 
     public GatesGame() {
         grid = new Space[3][20];
-        input = new Scanner(System.in);
         guesses = new String[] {"00", "01", "10", "11"};
         nonSpAnswers = new String[] {"000", "001", "010", "011", "100", "101", "110", "111"};
         guess = "NaN";
@@ -26,6 +36,34 @@ public class GatesGame {
         layers = 1;
         rows = 3;
         lives = 3;
+        frame = new JFrame("Gates Game");
+        frame.setVisible(true);
+        frame.setSize(new Dimension(500, 300));
+        frame.setLocation(new Point(300, 300));
+        text = new JTextArea("helloooo");
+        panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        frame.getContentPane().add(panel);
+        panel.add(text, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        text.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                switch (Character.toString(e.getKeyChar())) {
+                    case "q":
+                        guess = guesses[0];
+                        break;
+                    case "w":
+                        guess = guesses[1];
+                        break;
+                    case "e":
+                        guess = guesses[2];
+                        break;
+                    case "r":
+                        guess = guesses[3];
+                        break;
+                }
+            }
+        });
 
         for(int col = 0; col < grid[0].length; col++) {
             if(col >= 18 - layers && col < 18) {
@@ -80,26 +118,6 @@ public class GatesGame {
         }
     }
 
-    public void getNextGuess() {
-        String entered = input.nextLine();
-
-        switch (entered) {
-            case "q":
-                guess = guesses[0];
-                break;
-            case "w":
-                guess = guesses[1];
-                break;
-            case "e":
-                guess = guesses[2];
-                break;
-            case "r":
-                guess = guesses[3];
-                break;
-        }
-        getNextGuess();
-    }
-
     public boolean currentGuessCorrect() {
         return guess.equals(correctAnswer);
     }
@@ -117,16 +135,14 @@ public class GatesGame {
         if(getQubitPosition() == 17) {
             if(!currentGuessCorrect()) {
                 lives--;
-                System.out.println("Game Over!");
+                //System.out.println("Game Over!");
+                text.setText("Game Over!");
                 if(lives == 0) {
-                    System.exit(0);
+                    panel.remove(text);
                 }
             }
             spawnNewQubits();
         }
-    }
-
-    public void checkSpeedRequested() {
     }
 
     public void ticker() {
@@ -144,47 +160,68 @@ public class GatesGame {
 
     public void print() {
         //Sorry for the awful method, I just wanted a quick display :)
+        String newFrame = "";
+/*
         for(int i = 0; i < 100; i++) {
-            System.out.println();
+            //System.out.println();
+            newFrame += "\n";
         }
-        for(int i = 0; i < 43; i++) {
-            System.out.print("-");
+            */
+        for(int i = 0; i < 39; i++) {
+            //System.out.print("-");
+            newFrame += "-";
         }
-        System.out.println();
-        System.out.println("Correct answer (for testing): |" + correctAnswer + ">");
-        System.out.println("Lives Remaining: " + lives);
-        System.out.println("Q: |" + guesses[0] + ">, W: |" + guesses[1] + ">, E: |" + guesses[2] + ">, R: |" + guesses[3] + ">");
-        for(int i = 0; i < 43; i++) {
-            System.out.print("-");
+        //System.out.println();
+        newFrame += "\n";
+        //System.out.println("Correct answer (for testing): |" + correctAnswer + ">");
+        newFrame += "Correct answer (for testing): |" + correctAnswer + ">\n";
+        //System.out.println("Lives Remaining: " + lives);
+        newFrame += "Lives Remaining: " + lives + "\n";
+        //System.out.println("Q: |" + guesses[0] + ">, W: |" + guesses[1] + ">, E: |" + guesses[2] + ">, R: |" + guesses[3] + ">");
+        newFrame += "Q: |" + guesses[0] + ">, W: |" + guesses[1] + ">, E: |" + guesses[2] + ">, R: |" + guesses[3] + ">\n";
+        for(int i = 0; i < 39; i++) {
+            //System.out.print("-");
+            newFrame += "-";
         }
-        System.out.println();
+        //System.out.println();
+        newFrame += "\n";
         for(int i = 0; i < rows; i++) {
             Space[] row = grid[i];
-            System.out.print("|");
+            //System.out.print("|");
+            newFrame += "|";
             for(Space s : row) {
                 String symbol = s.getSymbol();
                 if(symbol.equals("0") || symbol.equals("1") || symbol.equals("+") || symbol.equals("-")) {
-                    System.out.print("|" + symbol + ">");
+                    //System.out.print("|" + symbol + ">");
+                    newFrame += "|" + symbol + ">";
                 } else {
-                    System.out.print(" " + s.getSymbol());
-                    if(s.getSymbol().equals("")) {
-                        System.out.print(" ");
+                    //System.out.print(" " + s.getSymbol());
+                    newFrame += " " + symbol;
+                    if(symbol.equals("")) {
+                        //System.out.print(" ");
+                        newFrame += " ";
                     }
                 }
             }
-            System.out.println(guess.equals("NaN") ? "|" : "|   |" + guess.substring(i, i + 1) + ">");
+            //System.out.println(guess.equals("NaN") ? "|" : "|   |" + guess.substring(i, i + 1) + ">");
+            newFrame += guess.equals("NaN") ? "|\n" : "|   |" + guess.substring(i, i + 1) + ">\n";
         }
-        for(int i = 0; i < 43; i++) {
-            System.out.print("-");
+        for(int i = 0; i < 39; i++) {
+            //System.out.print("-");
+            newFrame += "-";
         }
-        System.out.println();
+        //System.out.println();
+        newFrame += "\n";
+        text.setText(newFrame);
+    }
+
+    public void run() {
+        spawnNewQubits();
+        print();
+        ticker();
     }
 
     public static void main(String[] args) {
-        GatesGame game = new GatesGame();
-        game.spawnNewQubits();
-        game.print();
-        game.ticker();
-        game.getNextGuess();
+        EventQueue.invokeLater(new GatesGame());
     }
 }
