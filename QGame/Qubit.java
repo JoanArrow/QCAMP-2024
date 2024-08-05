fafecf5aaef7faf6e61d53fcfe81ff3961f3e3a0
SadieBurris <111394578+SadieBurris@users.noreply.github.com>
2024-08-04 18:52:32 -0700

    removed useless methods

--- QGame/Qubit.java
+++ QGame/Qubit.java
@@ -19,26 +19,6 @@ public class Qubit extends Space {
         this.state = state;
     }
 
-    public void notGate() {
-        state = state.equals("0") ? "1" : state.equals("1") ? "0" : state;
-    }
-
-    public void cNotGate(Qubit control) {
-        if(control.getSymbol().equals("1")) {
-            notGate();
-        } else if (!control.getSymbol().equals("0")) {
-            state = "+"; //up to change
-        } else {
-            System.out.println("Error on cNot gate. Control: " + control.getSymbol() + ", qubit: " + state);
-        }
-    }
-
-    public void swapGate(Qubit other) {
-        String temp = state;
-        state = other.getSymbol();
-        other.setState(temp);
-    }
-
     public boolean isSuperposition() {
         return state.equals("+") || state.equals("-");
     }
